286b5ac5953597c576d068130070e999f1da0207
Terry Kubalewski <terrykub@yahoo.com>
2022-12-13 15:59:47 -0600

    gotta save

---
+++ test_module.py
@@ -0,0 +1,16 @@
+def greet(name):
+    print(f"Hello {name.title()}, how are you today? I hope you are doing well.")
+
+def leave(name):
+    print(f"Goodbye {name.title()}. It was a pleasure spending time with you")
+
+
+
+
+
+
+
+greet('terry')
+leave('terry')
+
+
